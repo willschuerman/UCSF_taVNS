c9161c086c93bfbb911e99375b555142cc284e42
Will <wschuerm@gmail.com>
2022-08-18 14:44:56 -0700

    Updated physiology to include general heart rate metrics

--- Code/Python/Python_analysis/analyze_physiology.py
+++ Code/Python/Python_analysis/analyze_physiology.py
@@ -12,7 +12,35 @@ import seaborn as sns
 os.chdir('../../..')
 os.chdir('Data')
 #%%
-filename = 'tVNS_DH_HRV analysis.xlsx'
+filename = 'tVNS_AC_HRV analysis.xlsx'
+
+
+# %%
+data = pd.read_excel('Physiology_Tests/'+filename,sheet_name='HRV Stats',nrows=26,index_col=0).transpose()
+data['Block'] = pd.Series()
+data.loc[data.index<6,'Block'] = 'Baseline'
+data.loc[(data.index>5) & (data.index<11),'Block'] = 'Stim1'
+data.loc[(data.index>10) & (data.index<16),'Block'] = 'Washout1'
+data.loc[(data.index>15) & (data.index<21),'Block'] = 'Stim2'
+data.loc[(data.index>20) & (data.index<26),'Block'] = 'Washout2'
+data.loc[(data.index>25) & (data.index<31),'Block'] = 'Stim3'
+
+data['BlockType'] = pd.Series()
+data.loc[data.index<6,'BlockType'] = 'Rest'
+data.loc[(data.index>5) & (data.index<11),'BlockType'] = 'Stim'
+data.loc[(data.index>10) & (data.index<16),'BlockType'] = 'Rest'
+data.loc[(data.index>15) & (data.index<21),'BlockType'] = 'Stim'
+data.loc[(data.index>20) & (data.index<26),'BlockType'] = 'Rest'
+data.loc[(data.index>25) & (data.index<31),'BlockType'] = 'Stim'
+# %%
+datamelt = data[['Mean Heart Rate','RSA','Mean IBI','SDNN','RMSSD','NN50','pNN50','Block','BlockType']].melt(id_vars=['Block','BlockType'])
+# %%
+datamelt['Variable'] = datamelt['Segment Number']
+sns.catplot(
+    data=datamelt, x='Block', y='value',
+    col='Variable', kind='box', col_wrap=3,sharey=False,
+    palette = ['b','r','b','r','b','r']
+)
 
 # %%
 data = pd.read_excel('Physiology_Tests/'+filename,sheet_name='Power Band Stats')
