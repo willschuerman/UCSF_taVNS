f9701154c36a626b2b9d5e661ca9802cc08a4d50
Will <wschuerm@gmail.com>
2022-11-07 14:48:14 -0800

    adding more event-related metrics

--- Code/R/SART_quick_single_analysis.R
+++ Code/R/SART_quick_single_analysis.R
@@ -7,7 +7,6 @@ myPalette <- c("#8961b3","#b55960","#999c47")
 
 
 # define custom functions
-
 adjust_idx <- function(idx,btrial){
   # check that idx isn't greater than limits of vector
   idx_start = idx-4
@@ -18,13 +17,13 @@ adjust_idx <- function(idx,btrial){
   if(idx_stop>length(btrial)){
     idx_stop=length(btrial)
   }
-  # check that idx doesn't cross block boundaries
-  while(btrial[idx_start]>btrial[idx]){
-    idx_start=idx_start+1
-  }
-  while(btrial[idx_stop]<btrial[idx]){
-    idx_stop=idx_stop-1
-  }    
+  # # check that idx doesn't cross block boundaries (changed to looping over blocks)
+  # while(btrial[idx_start]>btrial[idx]){
+  #   idx_start=idx_start+1
+  # }
+  # while(btrial[idx_stop]<btrial[idx]){
+  #   idx_stop=idx_stop-1
+  # }    
   return(c(idx_start,idx_stop))
 }
 
@@ -42,58 +41,58 @@ get_event_matrix <- function(idx_vector,data_vector,btrial){
   return(event_matrix)
 }
 
+make_event_df <- function(idx,RT,btrial,matrix_names,event_type){
+  if(!is_empty(idx)){
+    RT = get_event_matrix(idx,RT,btrial)
+    event_df <- data.frame(RT)
+    names(event_df) <- matrix_names
+    event_df <- reshape2::melt(event_df)
+    event_df$type <- event_type
+  }else{
+    event_df <- data.frame(matrix(ncol=3,nrow=0))
+    names(event_df) <- c('variable','value','type')
+  }
+  return(event_df)
+}
+
 # need to add block ids
 calc_event_metrics <- function(accuracy,RT,anticipations,comissions,omissions,btrial){
   matrix_names <- c('t-4','t-3','t-2','t-1','t0','t+1','t+2','t+3','t+4')
-  
-  
+
   # find ids of errors and collect RTs
-  error_idx = which(accuracy==0)
-  if(!is_empty(error_idx)){
-    error_RT = get_event_matrix(error_idx,RT,btrial)
-    event_data <- data.frame(error_RT)
-    names(event_data) <- matrix_names
-    event_data <- reshape2::melt(event_data)
-    event_data$type <- 'error'
-  }else{
-    event_data <- data.frame(matrix(ncol=3,nrow=0))
-    names(event_data) <- c('variable','value','type')
-  }
+  idx = which(accuracy==0)
+  event_data <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
   
-  # could add hits to targets
+  # find ids of correct trials and collect RTs
+  idx = which(accuracy==1)
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
+  
+  # find ids of go trials and collect RTs
+  idx = which(trialcode=='go')
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
   
   # find ids of anticipations and collect RTs
-  anticipations_idx = which(anticipations==1)
-  if(!is_empty(anticipations_idx)){
-    ant_RT = get_event_matrix(anticipations_idx,RT,btrial)
-    tmp <- data.frame(ant_RT)
-    names(tmp) <- matrix_names
-    tmp <- reshape2::melt(tmp)
-    tmp$type <- 'anticipation'
-    event_data <- rbind(event_data,tmp)
-  }
+  idx = which(trialcode=='nogo')
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
+  
+  # find ids of anticipations and collect RTs
+  idx = which(anticipations==1)
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
+  
 
   # find ids of comissions and collect RTs
-  comissions_idx = which(comissions==1)
-  if(!is_empty(comissions_idx)){
-    com_RT = get_event_matrix(comissions_idx,RT,btrial)
-    tmp <- data.frame(com_RT)
-    names(tmp) <- matrix_names
-    tmp <- reshape2::melt(tmp)
-    tmp$type <- 'comission'
-    event_data <- rbind(event_data,tmp)
-  }
+  idx = which(comissions==1)
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
   
   # find ids of omissions and collect RTs
-  omissions_idx = which(omissions==1)
-  if(!is_empty(omissions_idx)){
-    om_RT = get_event_matrix(omissions_idx,RT,btrial)
-    tmp <- data.frame(om_RT)
-    names(tmp) <- matrix_names
-    tmp <- reshape2::melt(tmp)
-    tmp$type <- 'omission'
-    event_data <- rbind(event_data,tmp)
-  }
+  idx = which(omissions==1)
+  tmp <-  make_event_df(idx,RT,btrial,matrix_names,event_type)
+  event_data <- rbind(event_data,tmp)
   
   # clean up data frame
   names(event_data) <- c('time','RT','type')
@@ -228,7 +227,21 @@ RT.summary <-data %>%
 
 #### visualize summary metrics ####
 
-ggplot(accuracy.summary[accuracy.summary$blocknum>0,],
+ggplot(accuracy.summary[accuracy.summary$blocknum>0 & 
+                          accuracy.summary$variable %in%
+                          c("n_targets","n_distractors",'total'),],
+       aes(x=blocknum,y=value,fill=variable))+
+  geom_bar(stat='identity',position=position_dodge())+
+  ggpubr::theme_pubclean()+
+  scale_fill_viridis_d(end=0.8,labels=c('NOGO', 'GO', 'Total'))
+
+
+ggplot(accuracy.summary[accuracy.summary$blocknum>0 & 
+                          accuracy.summary$variable %in%
+                          c("hits", "misses", "false_alarms", 
+                            "correct_rejections","hit_rate", "miss_rate", 
+                            "fa_rate", "hit_rate_adjusted", "fa_rate_adjusted", 
+                            "dprime", "dprime_adjusted", "aprime"),],
        aes(x=blocknum,y=value,color=subject,group=subject,shape=stimblock))+
   geom_point()+
   geom_line()+
@@ -240,12 +253,15 @@ ggplot(accuracy.summary[accuracy.summary$blocknum>0,],
 #### visualize event-related metrics ####
 
 d <- summarySE(event_data,measurevar='RT',groupvars=c('time','type','stimblock','subject'),na.rm=T)
+d$type <- as.factor(d$type)
+levels(d$type) <- c("anticipation error", "comission error", "all errors", "omission error")
+d$type <- relevel(d$type,'all errors')
 ggplot(d,aes(x=time,y=RT,color=stimblock,group=stimblock))+
   geom_vline(xintercept='t0')+
   geom_point()+
   geom_errorbar(aes(ymin=RT-ci,ymax=RT+ci),width=0.2)+
   geom_line()+
-  facet_grid(type~subject,scales='free')+
+  facet_grid(subject~type,scales='free')+
   ggpubr::theme_pubclean()+
   scale_color_manual(values=myPalette)
 
@@ -261,13 +277,14 @@ ggplot(d[d$blocknum>0,],aes(x=blocknum,y=values.RT,shape=stimblock,color=subject
   scale_color_manual(values=myPalette)
 
 d <- summarySE(data[data$blocknum>0,],measurevar='values.RT',groupvars=c('blocknum','stimblock','subject','trialcode'))
-ggplot(d[d$blocknum>0,],aes(x=blocknum,y=values.RT,shape=stimblock,color=subject,group=subject))+
-  geom_point(size=3)+
-  geom_line()+
-  geom_errorbar(aes(ymin=values.RT-ci,ymax=values.RT+ci),width=0.1)+
+ggplot(d[d$blocknum>0,],aes(x=blocknum,y=values.RT,color=stimblock,group=trialcode,shape=trialcode))+
+  geom_point(size=3,position=position_dodge(0.3))+
+  geom_errorbar(aes(ymin=values.RT-ci,ymax=values.RT+ci),width=0.1,
+                position=position_dodge(0.3))+
   ggpubr::theme_pubclean()+
   scale_color_manual(values=myPalette)+
-  facet_wrap('trialcode')
+  facet_wrap('subject')+
+  ylab('Reaction Time (ms)')
 
 d <- summarySE(data,measurevar='correct',groupvars=c('blocknum','stimblock','subject'))
 ggplot(d[d$blocknum>0,],aes(x=blocknum,y=correct,color=subject,shape=stimblock,group=subject))+
@@ -279,13 +296,13 @@ ggplot(d[d$blocknum>0,],aes(x=blocknum,y=correct,color=subject,shape=stimblock,g
   ylab('accuracy')
 
 d <- summarySE(data[data$blocknum>0,],measurevar='correct',groupvars=c('blocknum','stimblock','trialcode','subject'))
-ggplot(d[d$blocknum>0,],aes(x=blocknum,y=correct,color=subject,group=subject,shape=stimblock))+
-  geom_point(size=3)+
-  geom_line()+
-  geom_errorbar(aes(ymin=correct-ci,ymax=correct+ci),width=0.1)+
+ggplot(d[d$blocknum>0,],aes(x=blocknum,y=correct,color=stimblock,group=trialcode,shape=trialcode))+
+  geom_point(size=3,position=position_dodge(0.3))+
+  geom_errorbar(aes(ymin=correct-ci,ymax=correct+ci),width=0.1,
+                position=position_dodge(0.3))+
   ggpubr::theme_pubclean()+
   scale_color_manual(values=myPalette)+
-  facet_wrap('trialcode')+
+  facet_wrap('subject')+
   ylab('accuracy')
 
 
